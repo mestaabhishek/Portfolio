986d2b2c82275f16cedc6d54a88f9dffffe5413a
Abhishek <mestaabhishek2@gmail.com>
2026-02-08 01:17:18 +0530

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ export default [
   {
     title: 'SOS Game – Android',
     description: 'Published Android game available on Google Play Store.',
-    icon: '/icons/sos_logo.png',
+    icon: './icons/sos_logo.png',
     tech: ['Android Studio', 'Java'],
     links: [
       {
@@ -14,14 +14,14 @@ export default [
   {
     title: 'Pneumonia Detection using Deep Learning',
     description: 'Research project focused on medical image classification.',
-    icon: '/icons/pneumonia_logo.png',
+    icon: './icons/pneumonia_logo.png',
     tech: ['Python', 'TensorFlow', 'Keras', 'Inception V3 Model', 'Google Colab'],
     links: []
   },
   {
     title: 'ML Product Exploration Tool',
     description: 'Hackathon finalist project for product discovery using ML.',
-    icon: '/icons/product_exploration_logo.png',
+    icon: './icons/product_exploration_logo.png',
     tech: ['Python', 'Machine Learning'],
     links: []
   }
